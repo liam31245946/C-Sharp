07bdcc347776724b1a3964cf9336e7d1bd3a7c36
liam nguyen <130803077+liam31245946@users.noreply.github.com>
2025-02-28 14:46:12 -0800

    sorting-searching

---
+++ Sorting-Searching/Program.cs
@@ -0,0 +1,16 @@
+﻿﻿var fruits = new List <string>{"apple", "watermelon", "banana", "peach"};
+var numbers = new List<int>{ 3,45,56,23,94,345,1324,53};
+
+
+fruits.Sort();
+
+numbers.Sort();
+
+
+foreach (var fruit in fruits){
+    Console.WriteLine($"Hello {fruit.ToUpper()}");
+}
+
+foreach (var number in numbers){
+    Console.WriteLine(number);
+}
