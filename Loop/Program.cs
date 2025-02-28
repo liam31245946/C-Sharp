4d1906429d165eae3714065b0f4f4b6dc7a34b1a
liam nguyen <130803077+liam31245946@users.noreply.github.com>
2025-02-27 18:40:40 -0800

    Loop

---
+++ Loop/Program.cs
@@ -0,0 +1,18 @@
+﻿int i = 0;
+while ( i <= 5){
+    
+    Console.WriteLine(i++);
+
+}
+
+for( int a = 0; a <= 5; a++){
+    Console.WriteLine(a);
+}
+
+for (int row = 1; row < 11; row++)
+{
+    for (char column = 'a'; column < 'k'; column++)
+    {
+        Console.WriteLine($"The cell is ({row}, {column})");
+    }
+}
