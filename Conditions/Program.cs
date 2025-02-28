dd3447be40e8ec8577c7bc4673415212c60e07c3
liam nguyen <130803077+liam31245946@users.noreply.github.com>
2025-02-27 18:14:28 -0800

    Conditions

---
+++ Conditions/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if ( num < 10 ){
+    Console.WriteLine("the answer is smaller than 10");
+} else if (num>10){
+    Console.WriteLine(" The answer is greater than 10 ");
+} else {
+    Console.WriteLine("the answer is equal to 10");
+}
+
